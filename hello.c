e1f1020defd0d9bee72f581e6c7369e4350825d4
V. Hunter Adams <vha3@cornell.edu>
2017-09-24 13:21:02 -0400

    Continued refactor

--- hello.c
+++ hello.c
@@ -17,8 +17,6 @@
 //#include <ti/display/Display.h>
 #include <ti/drivers/Power.h>
 #include <ti/drivers/power/PowerCC26XX.h>
-//#include <ti/drivers/PIN.h>
-//#include <ti/drivers/pin/PINCC26XX.h>
 
 /* BIOS Header files */
 #include <ti/sysbios/BIOS.h>
@@ -59,23 +57,6 @@ static uint8_t AddressList[0x10] =
 /* Display Handle */
 //static Display_Handle display;
 
-///* Pin handles and states*/
-//static PIN_Handle pinHandle;
-//static PIN_State pinState;
-
-
-///*
-// * Application button pin configuration table:
-// *   - Interrupts are configured to trigger on rising edge.
-// */
-//PIN_Config pinTable[] = {
-//	CC1310_LAUNCHXL_DIO15  | PIN_INPUT_EN | PIN_PULLDOWN | PIN_IRQ_POSEDGE,
-//	CC1310_LAUNCHXL_DIO12  | PIN_INPUT_EN | PIN_PULLDOWN | PIN_IRQ_POSEDGE,
-//	CC1310_LAUNCHXL_DIO22  | PIN_INPUT_EN | PIN_PULLDOWN | PIN_IRQ_POSEDGE,
-//	CC1310_LAUNCHXL_PIN_RLED | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,
-//	CC1310_LAUNCHXL_PIN_GLED | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,
-//    PIN_TERMINATE
-//};
 
 /* Task structs */
 Task_Struct initializationTask;
@@ -411,43 +392,12 @@ Void gpsFunc(UArg arg0, UArg arg1)
 	}
 }
 
-
-//void pinCallback(PIN_Handle handle, PIN_Id pinId) {
-////    uint32_t currVal = 0;
-//	switch (pinId) {
-//		case CC1310_LAUNCHXL_DIO12:
-////			currVal =  PIN_getOutputValue(Board_PIN_LED0);
-////			PIN_setOutputValue(pinHandle, Board_PIN_LED0, !currVal);
-//			Semaphore_post(gyroSemaphoreHandle);
-//			break;
-//
-//		case CC1310_LAUNCHXL_DIO15:
-////			currVal =  PIN_getOutputValue(Board_PIN_LED1);
-////			PIN_setOutputValue(pinHandle, Board_PIN_LED1, !currVal);
-//			Semaphore_post(magSemaphoreHandle);
-//			break;
-//
-//		case CC1310_LAUNCHXL_DIO22:
-////			currVal =  PIN_getOutputValue(Board_PIN_LED1);
-////			PIN_setOutputValue(pinHandle, Board_PIN_LED1, !currVal);
-//			Semaphore_post(accelSemaphoreHandle);
-//			break;
-//
-//		default:
-//			/* Do nothing */
-//			break;
-//	}
-//}
-
 /*
  *  ======== main ========
  *
  */
 int main(void)
 {
-
-//	Clock_Params clkParams;
-
 	/* Initialize TI drivers */
     Board_initGeneral();
     I2C_init();
@@ -526,21 +476,6 @@ int main(void)
 	Task_construct(&gpsTask, gpsFunc,
 				   &task_params, NULL);
 
-	/* Create Semaphores */
-
-
-
-//    pinHandle = PIN_open(&pinState, pinTable);
-//	if(!pinHandle) {
-//		/* Error initializing button pins */
-//		while(1);
-//	}
-//
-//    /* Setup callback for button pins */
-//    if (PIN_registerIntCb(pinHandle, &pinCallback) != 0) {
-//        /* Error registering button callback function */
-//        while(1);
-//    }
 
     /* Start kernel. */
     BIOS_start();
